07cc5b0bba071e33d0084ebe3a619add79493b37
Chandan Sitlani <sitlanic@gmail.com>
2019-08-06 18:38:27 +0530

    added schema

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 var express=require('express'),
  bodyParser=require('body-parser'),
 mongoose=require('mongoose'),
-ejs=require('ejs'),
+
 crypto=require('crypto'),
 app=express();
+
+
 app.set('view engine','ejs');
+
+
 mongoose.connect("mongodb://localhost:27017/node_test",{useNewUrlParser:true});
 var User =require('./models/user.js'); 
 var db=mongoose.connection;
@@ -17,6 +21,9 @@ res.render('dashboard');
 else
 res.render('login');
 });
-app.get('/signup',(err,res)=>{if(!err)
+app.get('/signup',(req,res)=>{if(req)
 res.render('signup');});
+app.post('/signup',(req,res)=>{if(req)
+User.register({"name":req.body.name,"email":req.body.email,"password":req.body.password});
+});
 app.listen(8000,function(err){console.log("Serving on port 8000");});
