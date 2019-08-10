8c840ed992567dff6cc4ee23a2a04aeab99e832b
Chandan Sitlani <sitlanic@gmail.com>
2019-08-10 13:56:03 +0530

    just

--- server.js
+++ server.js
@@ -9,25 +9,36 @@ app.use(bodyParser.urlencoded({ limit: "50mb", extended: true, parameterLimit: 5
 
 app.set('view engine','ejs');
 
+mongoose.set('useCreateIndex', true);
 
 mongoose.connect("mongodb://localhost:27017/node_test",{useNewUrlParser:true});
+
+//import model
 var User =require('./models/user'); 
 var db=mongoose.connection;
+
 db.on('error',function(err){
 console.log(err);});
 db.once('open',function(){console.log("database connected");});
+
+//routes
 app.get('/',(req,res)=>{
 if(req.user)
 res.render('dashboard');
 else
 res.render('login');
 });
+
+
 app.get('/signup',function(req,res){
 res.render('signup');});
 
 
 app.post('/signup',function(req,res){
 	console.log(req);
-User.create(new User({name:req.body.name,email:req.body.email,"password":req.body.password}),(err,user)=>{res.render('registered');});
+	var pass=crypto.pbkdf2Sync(req.body.password,1000, 128, `sha512`).toString(`hex`);
+User.create(new User({name:req.body.name,email:req.body.email,"password":req.body.password}),(err,user)=>{res.render('dashboard');});
 });
+
+
 app.listen(8000,function(err){console.log("Serving on port 8000");});
